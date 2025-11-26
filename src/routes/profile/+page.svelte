3c0bcb47c9a381d40d64efa65739c587cab905b3
arc360 <voidyt16@gmail.com>
2025-11-26 00:07:42 -0600

    Forgot to uncomment this

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1918,8 +1918,6 @@ Promise.all([
     <div style="width: 100%; overflow: hidden; padding: 16px; box-sizing: border-box;">
         <div class="comments-section" style="width: 100%; overflow: hidden; box-sizing: border-box; padding: 0;">
             <!-- Comments Toggle (only for profile owner and admins) -->
-
-            <!--
             {#if canToggleComments}
                 <div class="comments-toggle">
                     <button 
@@ -1943,7 +1941,6 @@ Promise.all([
                     </button>
                 </div>
             {/if}
-                -->
             <!-- New Comment Box -->
             {#if loggedIn && commentsEnabled}
                 <div class="comment-input-box">
