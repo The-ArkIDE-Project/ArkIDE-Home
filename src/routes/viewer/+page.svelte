d6b6c6bc61f998509ae280454009858a0f0ef7d3
arc360 <voidyt16@gmail.com>
2026-01-11 16:44:38 -0600

    Remove border on project embed because it didnt look very good

--- src/routes/viewer/+page.svelte
+++ src/routes/viewer/+page.svelte
@@ -398,12 +398,7 @@ function parseContent(text) {
         left: 0;
         width: 100%;
         height: 100%;
-        border: 1px solid rgba(0, 0, 0, 0.15);
         border-radius: 8px;
-    }
-
-    :global(body.dark-mode) .project-embed {
-        border-color: rgba(255, 255, 255, 0.15);
     }
 
     .sidebar {
