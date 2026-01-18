25d59ff9a06fb1b2d30e2dca759786a0f85f365d
arc360 <voidyt16@gmail.com>
2026-01-18 17:07:43 -0600

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: false
-    })
-  }
+	kit: {
+		adapter: adapter({
+			runtime: 'nodejs22.x'
+		})
+	}
 };
 
 export default config;
