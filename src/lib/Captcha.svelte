cec91a19d192d498c558c928bdaa69ddefe2939a
arc360alt <voidyt16@gmail.com>
2026-02-26 23:32:24 -0600

    Switch to hcaptcha as cloudflare turnstile bad

--- src/lib/Captcha.svelte
+++ src/lib/Captcha.svelte
@@ -1,80 +1,69 @@
 <script>
-    import { page } from '$app/stores';
-    import { browser } from "$app/environment";
-    import { onMount, createEventDispatcher } from "svelte";
+  import { browser } from "$app/environment";
+  import { onMount, createEventDispatcher } from "svelte";
+  import { PUBLIC_CAPTCHA_ENABLED, PUBLIC_CAPTCHA_SITEKEY } from "$env/static/public";
 
-    import { PUBLIC_CAPTCHA_ENABLED } from "$env/static/public";
-    
-    const dispatch = createEventDispatcher();
-    
-    let mockCompleted = false;
-    let mockExpired = false;
-    const mockComplete = () => {
-        mockCompleted = true;
-        dispatch("update", "captcha_disabled");
-    };
-    const mockExpire = () => {
-        mockExpired = true;
-        dispatch("update", false);
-    };
-    const mockError = () => {
-        mockCompleted = false;
-        mockExpired = false;
-        dispatch("update", false);
-    };
+  const dispatch = createEventDispatcher();
+  let mockCompleted = false;
+  let mockExpired = false;
 
-    onMount(() => {
-        if (String(PUBLIC_CAPTCHA_ENABLED) === "false") {
-            return;
-        }
+  const mockComplete = () => {
+    mockCompleted = true;
+    dispatch("update", "captcha_disabled");
+  };
+  const mockExpire = () => {
+    mockExpired = true;
+    dispatch("update", false);
+  };
+  const mockError = () => {
+    mockCompleted = false;
+    mockExpired = false;
+    dispatch("update", false);
+  };
 
-        window.on_captcha_complete = (token) => {
-            dispatch("update", token);
-        };
+  onMount(() => {
+    if (String(PUBLIC_CAPTCHA_ENABLED) === "false") return;
 
-        window.on_captcha_expired = () => {
-            dispatch("update", false);
-        };
-
-        window.on_captcha_error = () => {
-            // reload the captcha
-            turnstile.reset();
-            dispatch("update", false);
-        };
-    });
+    window.on_captcha_complete = (token) => dispatch("update", token);
+    window.on_captcha_expired = () => dispatch("update", false);
+    window.on_captcha_error = () => dispatch("update", false);
+  });
 </script>
 
 <svelte:head>
-    {#if String(PUBLIC_CAPTCHA_ENABLED) !== "false"}
-        <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
-    {/if}
+  {#if String(PUBLIC_CAPTCHA_ENABLED) !== "false"}
+    <script
+      src="https://js.hcaptcha.com/1/api.js"
+      async
+      defer
+    ></script>
+  {/if}
 </svelte:head>
 
 {#if String(PUBLIC_CAPTCHA_ENABLED) === "false"}
-    <div style="border: 1px solid black; padding: 8px;">
-        <span style="font-size: 24px">
-            {#if mockExpired}
-                You took too long
-            {:else if mockCompleted}
-                ✅ Success!
-            {:else}
-                <button style="border: 1px solid black; padding: 14px;" on:click={mockComplete}>X</button>
-                I'm not a robot
-            {/if}
-        </span>
-        <br>
-        <br>
-        <button on:click={mockExpire}>Expire</button>
-        <button on:click={mockError}>Error (Reset)</button>
-        <br>
-        Emulator, Captcha is disabled in .env, see PUBLIC_CAPTCHA_ENABLED
-    </div>
+  <div style="border: 1px solid black; padding: 8px;">
+    <span style="font-size: 24px">
+      {#if mockExpired}
+        You took too long
+      {:else if mockCompleted}
+        ✅ Success!
+      {:else}
+        <button style="border: 1px solid black; padding: 14px;" on:click={mockComplete}>X</button>
+        I'm not a robot
+      {/if}
+    </span>
+    <br /><br />
+    <button on:click={mockExpire}>Expire</button>
+    <button on:click={mockError}>Error (Reset)</button>
+    <br />
+    Emulator, Captcha is disabled in .env, see PUBLIC_CAPTCHA_ENABLED
+  </div>
 {:else}
-    <div
-        class="cf-turnstile"
-        data-sitekey="0x4AAAAAACCGRUg6dAwVN1Ai"
-        data-callback="on_captcha_complete"
-        data-expired-callback="on_captcha_expired"
-        data-error-callback="on_captcha_error"
-    />
+  <div
+    class="h-captcha"
+    data-sitekey={PUBLIC_CAPTCHA_SITEKEY}
+    data-callback="on_captcha_complete"
+    data-expired-callback="on_captcha_expired"
+    data-error-callback="on_captcha_error"
+  />
 {/if}
